17fca058beb7eb714e7fe20ca1e1ba6dc10ba9a3
bywwh <1065449148@qq.com>
2016-01-06 09:22:07 +0800

    default sdcard mixed [1/2]

--- build.sh
+++ build.sh
@@ -11,17 +11,15 @@ echo
 flyme clean
 flyme fullota
 
-if [ -e $FLYME_OUT_DIR/flyme*.zip ];then
-mv -vf $FLYME_OUT_DIR/flyme*.zip $FLYME_OUT_DIR/FlymeOS_aries-$USER-$(date +%Y%m%d)-5.1.1.zip
-fi
-
 echo
 echo ">>> make merge partition patch"
 echo
 cp -v -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/boot.img.out.bak
 cp -v -rf $DEVICE_DIR/framework-res $DEVICE_DIR/framework-res.bak
+cp -v -rf $DEVICE_DIR/overlay/META-INF $DEVICE_DIR/overlay/META-INF.bak
 cp -v -rf $OTHER_DIR/storage_list.xml $DEVICE_DIR/framework-res/res/xml
 cp -v -rf $OTHER_DIR/fstab.qcom $OTHER_DIR/init.target.rc $DEVICE_DIR/boot.img.out/RAMDISK
+cp -v -rf $OTHER_DIR/META-INF $DEVICE_DIR/overlay
 echo
 echo ">>> make fullota again"
 echo
@@ -31,11 +29,13 @@ cd $DEVICE_DIR
 flyme clean
 flyme fullota
 rm -v -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/framework-res
+rm -v -rf $DEVICE_DIR/overlay/META-INF
 mv -vf $DEVICE_DIR/boot.img.out.bak $DEVICE_DIR/boot.img.out
 mv -vf $DEVICE_DIR/framework-res.bak $DEVICE_DIR/framework-res
+mv -vf $DEVICE_DIR/overlay/META-INF.bak $DEVICE_DIR/overlay/META-INF
 
 if [ -e $FLYME_OUT_DIR/flyme*.zip ];then
-mv -vf $FLYME_OUT_DIR/flyme*.zip $FLYME_OUT_DIR/FlymeOS_aries-$USER-$(date +%Y%m%d)-mixed-5.1.1.zip
+mkdir -vp $FLYME_OUT_DIR/std && mv -vf $FLYME_OUT_DIR/flyme*.zip $FLYME_OUT_DIR/std
 fi
 
 if [ -e $DEVICE_DIR/tmp/Flyme*.zip ] || [ -e $DEVICE_DIR/tmp/target*.zip ];then
